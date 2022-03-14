cae7aa5a67ea56dcdd89fc71eb1373bfbc2b9191
bulchack <jesuseovalles@gmail.com>
2022-03-14 15:13:55 -0300

    add entities relations

--- src/wearable/wearableType.entity.ts
+++ src/wearable/wearableType.entity.ts
@@ -1,5 +1,6 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, OneToMany } from "typeorm";
 import { BaseEntity } from "../base-entity";
+import { Wearable } from "./wearable.entity";
 
 @Entity('wearable_types')
 export class WearableType extends BaseEntity {
@@ -8,4 +9,7 @@ export class WearableType extends BaseEntity {
 
     @Column({nullable: true})
     description: string;
+
+    @OneToMany(type => Wearable, wearable => wearable.wearableType)
+    weareables: Wearable[]
 }
