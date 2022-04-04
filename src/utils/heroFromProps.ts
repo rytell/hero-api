ab974c6062d1f0ea7474ec3fcbdb9badbaa103ca
Pedro Medina Faria <pedromedina190@gmail.com>
2022-04-04 12:51:30 -0400

    add owner to props processing

--- src/utils/heroFromProps.ts
+++ src/utils/heroFromProps.ts
@@ -1,8 +1,9 @@
 export const heroFromProps = (heroProps: any[]) => {
   return {
-    staked: heroProps[0],
-    lastStaked: heroProps[1],
-    lastUnstaked: heroProps[2],
-    heroId: heroProps[3],
-  };
+        staked: heroProps[0],
+        lastStaked: heroProps[1],
+        lastUnstaked: heroProps[2],
+        heroId: heroProps[3],
+        owner: heroProps[4],
+    };
 };
