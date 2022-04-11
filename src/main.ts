8b2c0f770a8dd37b1a8f186a3b0f6d4e91939ab9
Pedro Medina Faria <pedromedina190@gmail.com>
2022-04-11 09:22:38 -0400

    uncomment syncer

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
-// import { syncDb } from './dbUpdater';s
+import { syncDb } from './dbUpdater';
 
 async function bootstrap() {
     dotenv.config();
@@ -9,7 +9,7 @@ async function bootstrap() {
     app.enableCors();
     await app.listen(process.env.PORT || 3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
-    // syncDb(app);
+    syncDb(app);
 }
 
 bootstrap();
