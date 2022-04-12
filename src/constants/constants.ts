9e2c8229600a908c273c0d07bd685435f4355689
Pedro Medina Faria <pedromedina190@gmail.com>
2022-04-12 13:02:30 -0400

    add hero contract at mainnet

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -62,7 +62,7 @@ export const RPC_URL = {
 export const STAKING_HERO = {
     // [ChainId.FUJI]: '0xf776AC03A70F8bD499F1CA6786Ec57e730401E0F',
     [ChainId.FUJI]: '0xc7bA3f3dC4D0eb18914Ce946e2295bA09c118989',
-    [ChainId.AVALANCHE]: '',
+    [ChainId.AVALANCHE]: '0xE0B05Eb50E8481e1685612cF0a16251Bc2f7A1b4',
 };
 
 export const RADI: { [chainId in ChainId]: Token } = {
