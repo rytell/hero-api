c1c76ec77c87ecff5e87c9a43229e03217ee38e7
Pedro Medina Faria <pedromedina190@gmail.com>
2022-04-13 10:54:03 -0400

    change mail receptor

--- src/utils/sender.ts
+++ src/utils/sender.ts
@@ -1,34 +1,32 @@
-import AWS from 'aws-sdk'
+import AWS from 'aws-sdk';
 
 const SES_CONFIG = {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: 'us-east-1',
-  };
+};
 
 const AWS_SES = new AWS.SES(SES_CONFIG);
 
 export const sendError = (error) => {
-      let params = {
+    const params = {
         Source: 'jesuseovalles@gmail.com',
         Destination: {
-          ToAddresses: [
-            'jesuseovalles@gmail.com',
-          ],
+            ToAddresses: ['claims@psidiumlabs.org'],
         },
         ReplyToAddresses: [],
         Message: {
-          Body: {
-            Html: {
-              Charset: 'UTF-8',
-              Data: error.toString(),
+            Body: {
+                Html: {
+                    Charset: 'UTF-8',
+                    Data: error.toString(),
+                },
             },
-          },
-          Subject: {
-            Charset: 'UTF-8',
-            Data: `Hero Staking API Error`,
-          }
+            Subject: {
+                Charset: 'UTF-8',
+                Data: `Hero Staking API Error`,
+            },
         },
-      };
-      return AWS_SES.sendEmail(params).promise();
     };
+    return AWS_SES.sendEmail(params).promise();
+};
